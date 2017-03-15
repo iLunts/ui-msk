161fd672fb6dad12fdf6069fa979d667430d2db2
User <User>
2017-03-16 01:34:58 +0300

    Buf stay in func for changeState.

--- view/modal/modalCtrl.js
+++ view/modal/modalCtrl.js
@@ -10,4 +10,13 @@ app.controller('modalCtrl', function($scope){
   $scope.onSuccess = function(e) {
     e.clearSelection();
   };
+
+  $scope.isOpenModalBlank = false;
+  // $scope.isOpenEventEdit = false;
+
+  $scope.changeState = function(stateItem){
+    stateItem = !stateItem;
+    console.log(stateItem);
+    return stateItem;
+  }
 });
